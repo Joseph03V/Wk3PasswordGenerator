351085e2185cb185f62307add8b0df3a341ed442
Joey V <joey03v@gmail.com>
2023-10-03 14:34:41 -0500

    added function to retrieve password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,38 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); //button
+
+//generate password function
+function generatePassword() {
+  //variables for the password criteria
+var uppercaseLetters ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercaseLetters ="abcdefghijklmnopqrstuvwxyz";
+var numbers ="0123456789";
+var specials ="!@#$%^&*()?{}[]";
+var
+var
+var
+var passwordLength;
+//Get the desired length of the password
+getLength();
+function getLength(){
+  passwordLength = window.prompt("How long do you want the passowrd? (Between 8 and 128)");
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Please enter a valid length. (8-128)");
+    getLength();
+  }
+}
+
+
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password"); //replaces the placeholder
 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', criteria());
